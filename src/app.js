e500265a56c6cc2c108b8912f3f53ea11efd80e5
mati.prieto98@gmail.com <mati.prieto98@gmail.com>
2021-10-13 16:30:14 -0300

    ordene el app.js

--- src/app.js
+++ src/app.js
@@ -1,43 +1,54 @@
+// Todos los require 
 const express = require("express");
 const session = require('express-session');
 const cookies = require('cookie-parser');
+const methodOverride = require("method-override");
 const path = require("path");
 
 const app = express();
-app.use(express.json());
-
 app.use(session({
     secret: "Mensaje Secreto",
     resave: false,
 	saveUninitialized: false,
 }));
-
 app.use(cookies());
+app.use(methodOverride("_method"));
+app.use(express.json());
 
-// const bodyParser = require('body-parser');
-// app.use(bodyParser.json());
-
+// Para poder ver las vistas
 const publicPath = path.resolve(__dirname, "../public");
 app.use(express.static(publicPath));
 
-const methodOverride = require("method-override");
-
-app.listen(3000, () => console.log("Servidor esta corriendo"));
-
+// Implementando EJS 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "./views"));
 
+// Rutas
 const indexRouter = require("./routes/index");
-const productsRouter = require('./routes/products'); 
+const productsRouter = require('./routes/products');
 
-app.use(methodOverride("_method"));
 app.use("/", indexRouter);
-app.use(express.static(path.join(__dirname, "../public")));
 app.use("/products",productsRouter)
 
-module.export = app;
+// Cargando el servidor
+app.listen(3000, () => console.log("Servidor esta corriendo"));
+
+
+
+module.export = app;    // nose que es esto.
+
+
+
+// --- Cosas comentadas --- //
+
+// const bodyParser = require('body-parser');
+// app.use(bodyParser.json());
 
 // ACA CARGO LA PAGINA DEL BUSCADOR
 // app.get("/", (req, res) => {
 //  res.sendFile(path.join(__dirname, "/views/home.html"));
 //});
+
+
+// Forma de llamar a vistas en public mas corta sin variable
+// app.use(express.static(path.join(__dirname, "../public")));
