3769448ce8c9fe2a43beda9d1a9e30c4efff4ca7
mati.prieto98@gmail.com <mati.prieto98@gmail.com>
2021-10-13 19:12:03 -0300

    base de Users: Rutas y controller

---
+++ src/controllers/userController.js
@@ -0,0 +1,30 @@
+// Todos los require
+const fs = require("fs");
+const path = require("path");
+const multer = require("multer");
+
+// completar
+
+const controller = {
+    register: (req, res) => {
+		return res.render('tutuni-inicio.registro');
+	},
+    processRegister: (req, res) => {
+
+    },
+    login: (req, res) => {
+		return res.render('futura pagina de login');
+	},
+    processLogin: (req,res) =>{
+
+    },
+    profile: (req, res) => {
+
+    },
+    logout: (req, res) =>{
+
+        return res.redirect('/');
+    }
+}
+
+module.exports = controller;
---
+++ src/routes/usersRoutes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const path = require("path");
+
+// Controlador
+const usersController = require('../controllers/userController');
+// Middlewares (todavia no se estan usando)
+const configMulter = require("../middlewares/productMulter")
+
+// Registro
+router.get('/register', usersController.register);
+
+// Procesando el registro
+router.post('/register', usersController.processRegister);
+
+// Login
+router.get('/login', usersController.login);
+
+// Procesando el login
+router.post('/login', usersController.processLogin);
+
+// Perfil Usuario
+router.get('/user-profile/', usersController.profile);
+
+// Logout
+router.get('/logout/', usersController.logout);
+
+module.exports = router;
