a132d69d775ee54df01884ae3e01abc1ef726737
mati.prieto98@gmail.com <mati.prieto98@gmail.com>
2021-12-17 16:46:51 -0300

    Corregi las validaciones del userValidator.js

--- src/middlewares/usersValidator.js
+++ src/middlewares/usersValidator.js
@@ -1,10 +1,10 @@
-const path = require("path");
 const { body } = require('express-validator');
+const db = require('../../database/models');
 
 const validations = [
     body('name')
         .notEmpty()
-        .withMessage("Debes completar el campo") 
+        .withMessage('Debe ingresar su nombre completo') 
         .bail()
         .isLength({min:4, max:20})
         .withMessage("Debe tener de 4 a 20 caracteres"), 
@@ -15,31 +15,56 @@ const validations = [
         .isLength({min:4, max:20})
         .withMessage("Debe tener de 4 a 20 caracteres"),
     body('birthdate')
-        .not().isEmpty(),
+        .not().isEmpty()
+        .withMessage("Debes ingresar una fecha"),
     body('email')
         .notEmpty()
-        .withMessage("Debes completar este casillero")
+        .withMessage('Debe ingresar algun e-mail')
         .bail()
         .isEmail()
-        .withMessage("Debes completar con una direccion de email"),
+        .withMessage('La dirección de e-mail ingresada no es válida')
+        .bail()
+        //   https://express-validator.github.io/docs/custom-validators-sanitizers.html
+        .custom((value, { req }) => {
+          return db.Users.findOne({
+            where: {
+              email: req.body.email,
+            },
+          }).then((user) => {
+            if (user) {
+              return Promise.reject('Este correo ya existe');
+            }
+          });
+        }),
     body('adress')
         .notEmpty()
         .withMessage("Debes completar el campo")
         .bail()
-        .isLength({min:4, max:30})
+        .isLength({ min:4, max:30 })
         .withMessage("Debe tener de 4 a 30 caracteres"),
     body('password')
         .notEmpty()
-        .withMessage("No escribiste")
+        .withMessage('Debe ingresar una contraseña')
         .bail()
-        .isLength({min:4, max:20})
-        .withMessage("Malisima, muy corta.. o muy larga"),
+        .isLength({ min:4 })
+        .withMessage("La contraseña debe tener por lo menos 4 caracteres")
+        .bail()
+        .custom((value, { req }) => {
+            if (value !== req.body.confirmPassword) {
+              throw new Error('Las contraseñas no coinciden');
+            }
+            return true;
+        }),
     body('confirmPassword')
         .notEmpty()
         .withMessage("Debes completar el campo")
         .bail()
         .isLength({min:4, max:20})
         .withMessage("No sirve"),
+]
+
+module.exports = validations;
+
     // body('avatar').custom((value, { req }) => {
     //     let acepted = ['.jpeg', '.png', '.gif'];
     //     let fileExtension = path.extname(file.originalname);
@@ -50,6 +75,3 @@ const validations = [
     //         return true
     //     }
     // })                     no estaria funcionando bien
-]
-
-module.exports = validations
