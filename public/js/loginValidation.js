ced86cc6783530049a54e37741274e61a816bb38
salvacarou <salvadorcarou@gmail.com>
2021-12-18 21:00:40 -0300

    login validations / se ven los errores en register

--- public/js/loginValidation.js
+++ public/js/loginValidation.js
@@ -1,11 +1,6 @@
-const name = document.querySelector("#name")
-const username = document.querySelector("#username")
+
 const email = document.querySelector("#email")
-const birthdate = document.querySelector("#birthdate")
-const adress = document.querySelector("#adress")
 const password = document.querySelector("#password")
-const confirmPassword = document.querySelector("#confirmPassword")
-const formulario = document.querySelector("form");
 
 const errorList = document.querySelector("#errors");
 
@@ -13,8 +8,8 @@ formulario.addEventListener("submit", (event) =>{
     const errors = formIsInvalid();
     if (errors.length > 0){
         console.log(errors)
-
     event.preventDefault()
+
     errorList.classList.remove("hidden");
 
     errorList.innerHTML = "";
@@ -33,13 +28,9 @@ function formIsInvalid(){
 
      let errors = [];
 
-    errors.push(validateInput(name, isEmpty, "El nombre esta mal"));
-    errors.push(validateInput(username, isEmpty, "El nombre de usuario esta mal"));
+
     errors.push(validateInput(email, isEmpty, "El Email esta mal"));
-    errors.push(validateInput(birthdate, isEmpty, "El cumpleanios esta mal"));
-    errors.push(validateInput(adress, isEmpty, "La direccion esta mal"));
     errors.push(validateInput(password, isEmpty, "La constraseña esta mal"));
-    errors.push(validateInput(confirmPassword, isEmpty, "La confirmacion de password esta mal"));
 
     return errors.filter((msg) => msg != null);
 }
