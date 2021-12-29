1727c30097c3211352bb913b9dbba14c249f5552
mati.prieto98@gmail.com <mati.prieto98@gmail.com>
2021-12-29 10:30:37 -0300

    agregue último producto vendido - falta img

--- dashbord/src/components/Home/ContentRowCenter.js
+++ dashbord/src/components/Home/ContentRowCenter.js
@@ -4,10 +4,9 @@ import ProductSale from './ProductSale';
 import ImagenProducto from "../../assets/images/mandalorian.jpg"
 import './Home.css';
 function ContentRowCenter() {
-  let textDescription = "La camiseta de fútbol para hombre 3° Recambio Stadium del FC Barcelona 2019/20 está confeccionada con tela transpirable Dri-Fit que absorbe el sudor y te mantiene fresco, seco y cómodo tanto dentro como fuera de la cancha."
   return (
     <div className="row">
-      <ProductSale img={ImagenProducto} description={textDescription} />
+      <ProductSale img={ImagenProducto} />
       <BranchList/>
     </div>
   );
--- dashbord/src/components/Home/ProductSale.js
+++ dashbord/src/components/Home/ProductSale.js
@@ -1,24 +1,59 @@
-import React from 'react';
+import React, { Component } from "react";
 import './Home.css';
+import { Link } from "react-router-dom"; 
 import ImageProduct from "../../assets/images/mandalorian.jpg"
 
-function ProductSale(props) {
-  return (
-    <div className="col-lg-6 mb-4">
+const productsURL = "/api/products";
+
+// Last Product
+class ProductSale extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {};
+      }
+
+    render() {
+    if (!this.state.lastProduct) {
+        return <div>Cargando</div>;
+    }
+    return(
+        <div className="col-lg-6 mb-4">
         <div className="card shadow mb-4">
             <div className="card-header py-3">
-                <h5 className="m-0 font-weight-bold text-gray-800">Producto más vendido</h5>
+                <h5 className="m-0 font-weight-bold text-gray-800">Último Producto</h5>
             </div>
             <div className="card-body">
                 <div className="text-center">
+                    <h2>{this.state.lastProduct.title}</h2>
                     <img className="img-fluid px-3 px-sm-4 mt-3 mb-4 imageProductHome" src={ImageProduct} alt="ImageProduct"/>
+                    {/* Falta traer la imagen */}
+                    {/* {this.state.lastProduct.imgURL} */}
                 </div>
-                <p>{props.description}</p>
+                <p>{this.state.lastProduct.description}</p>
                 <a className="btn btn-danger" target="_blank" rel="nofollow" href="/">Ver detalle producto</a>
             </div>
         </div>
     </div>
-  );
+    )
 }
 
+componentDidMount() {{}
+    console.log("CD-Mount");
+    this.LastProduct();
+}
+async LastProduct() {
+    const result = await fetch(productsURL);
+    const response = await result.json();
+    console.log(response)
+    const product = response.data.products;
+    // console.log(product)
+    
+    const lastProductId = product.length-1;
+    const lastProduct = product[lastProductId];
+    // console.log(lastProduct)
+
+    this.setState({ lastProduct: lastProduct });
+  }
+}
 export default ProductSale;
